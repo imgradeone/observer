565e1f4e410396fc70a5f41d9dd0c4a809b68d40
imgradeone <imgradeone@outlook.com>
2021-01-19 17:24:54 +0800

    sssssssssssccc

--- src/plugins/theme.js
+++ src/plugins/theme.js
@@ -1,13 +1,13 @@
 let date = new Date();
-let primaryColor = "#1e88e5";
+let primaryColor = "#fa4694";
 if (date.getDate() == 1 && date.getMonth() == 3) {
-  primaryColor = "#62c076";
+  primaryColor = "#fa4694";
 } else if (date.getDate() == 1 && date.getMonth() == 9) {
-  primaryColor = "#f55a4e";
+  primaryColor = "#fa4694";
 } else if (
   date.getDate() == 4 &&
   date.getMonth() == 3 &&
-  date.getFullYear() == 2020
+  date.getFullYear() == 2021
 ) {
   primaryColor = "#888";
 }
